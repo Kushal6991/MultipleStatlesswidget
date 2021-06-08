aeffff62a0e34a6a15dae2950c3e90aa967a672b
Kushal C <kratik.852@gmail.com>
2021-06-08 15:27:16 +0530

    fourth

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
 import 'dart:core';
-import 'dart:ffi';
-
-
-
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:intl/intl.dart';
 
-
 void main() => runApp(MyApp());
-
 
 class MyApp extends StatelessWidget {
   @override
@@ -31,8 +22,8 @@ class MyApp extends StatelessWidget {
 
 class DashboardLandingScreen extends StatelessWidget {
   static Route route() => MaterialPageRoute(
-    builder: (context) => DashboardLandingScreen(),
-  );
+        builder: (context) => DashboardLandingScreen(),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -59,96 +50,79 @@ class DashboardLandingScreen extends StatelessWidget {
 
 class ActivityScreen extends StatelessWidget {
   static Route route() => MaterialPageRoute(
-    builder: (context) => ActivityScreen(),
-  );
+        builder: (context) => ActivityScreen(),
+      );
   TextEditingController activename = TextEditingController();
   TextEditingController activetype = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: NestedScrollView(
-
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
-          return <Widget>[
-            CupertinoSliverNavigationBar(
-              largeTitle: Text('Create an activity'),
+        body: Container(
+            child: NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          CupertinoSliverNavigationBar(
+            largeTitle: Text('Create an activity'),
           ),
-          ];
-         },
-           body: SingleChildScrollView(
-              child: Column(
-              children: <Widget>[
-               Padding(
+        ];
+      },
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
               padding: EdgeInsets.only(top: 50.0),
               child: new TextField(
-                controller: activename ,
-                decoration: InputDecoration
-                  (
-                    labelText: "Activity name",
-                    labelStyle:TextStyle(color: Colors.black,
-                        fontStyle: FontStyle.normal,fontSize: 25),
-                    hintText: "Ex: Cooking Class",
-                    errorText: ( 'Name should be max 120 character'),
-               ),
+                controller: activename,
+                decoration: InputDecoration(
+                  labelText: "Activity name",
+                  labelStyle: TextStyle(
+                      color: Colors.black,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 25),
+                  hintText: "Ex: Cooking Class",
+                  errorText: ('Name should be max 120 character'),
+                ),
                 maxLength: 120,
               ),
-               ),
-               Padding(
-                padding: EdgeInsets.only(top: 50.0,bottom:200),
-                 child: new TextField(
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 50.0, bottom: 200),
+              child: new TextField(
                 controller: activetype,
-                decoration: InputDecoration
-              (
-                labelText: "Activity type",
-                labelStyle:TextStyle(color: Colors.black,
-                    fontStyle: FontStyle.normal,fontSize: 25),
-                hintText: "Ex: Cooking Class",
-                 ),
-                  ),
+                decoration: InputDecoration(
+                  labelText: "Activity type",
+                  labelStyle: TextStyle(
+                      color: Colors.black,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 25),
+                  hintText: "Ex: Cooking Class",
+                ),
               ),
-
-
-             new Align(
+            ),
+            new Align(
               alignment: Alignment.bottomRight,
               // ignore: deprecated_member_use
-               child: RaisedButton(
-
+              child: RaisedButton(
                 child: Text(' NEXT'),
-                 color: Colors.black,
-                 textColor: Colors.white,
-                onPressed: () =>
-                  Navigator.of(context).push(
+                color: Colors.black,
+                textColor: Colors.white,
+                onPressed: () => Navigator.of(context).push(
                   NewTopic.route(),
                 ),
-                ),
-           )
-            ],
-                ),
               ),
-      )
-      )
-    );
-    }
-}
- /*String? _validate(String userInput) {
-  if (!(userInput.length > 120) && userInput.isNotEmpty) {
-    return "correction required in activity name ";
+            )
+          ],
+        ),
+      ),
+    )));
   }
-  return null;
-
 }
-*/
+
 class NewTopic extends StatelessWidget {
-
-
-  static Route route() =>
-      MaterialPageRoute(
+  static Route route() => MaterialPageRoute(
         builder: (context) => NewTopic(),
       );
   @override
-
-
   TextEditingController Startdate = TextEditingController();
   TextEditingController Enddate = TextEditingController();
 
@@ -157,125 +131,104 @@ class NewTopic extends StatelessWidget {
     return Scaffold(
       body: Container(
           child: NestedScrollView(
-            headerSliverBuilder: (BuildContext context,
-                bool innerBoxIsScrolled) {
-              return <Widget>[
-                CupertinoSliverNavigationBar(
-                  largeTitle: Text('date of activity'),
-                ),
-              ];
-            },
-
-            body: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 50.0),
-                    child: new TextField(
-                      controller: Startdate,
-                      decoration: InputDecoration
-                        (
-                        labelText: "Start date",
-                        labelStyle: TextStyle(color: Colors.black,
-                            fontStyle: FontStyle.normal, fontSize: 25),
-                        hintText: 'Dec 02,2020',
-                      ),
-                     onTap:() async {
-                       DateTime? PickedDate = await showDatePicker(
-                           context: context,
-                           initialDate: DateTime.now(),
-                           firstDate: DateTime(2000),
-                           //DateTime.now() - not to allow to choose before today.
-                           lastDate: DateTime(2101)
-                       );
-                       if(PickedDate != null ) {
-                         print(PickedDate);  //pickedDate output format => 2021-03-10 00:00:00.000
-                         String formattedDate = DateFormat('yyyy-MM-dd').format(PickedDate) ;
-
-
-                        // print(formattedDate); //formatted date output using intl package =>  2021-03-16
-                         //you can implement different kind of Date Format here according to your requirement
-                         Startdate.text = formattedDate;
-                         //PickedDate=info ;
-                           (context as Element).markNeedsBuild();//set output date to TextField value.
-                       }else{
-                         print("Date is not selected");
-                       }
-                       },
-                    ),
-                  ),
-                    Padding(
-                    padding: EdgeInsets.only(top: 50.0, bottom: 200),
-                    child: new TextField(
-                      controller: Enddate,
-                      decoration: InputDecoration
-                        (
-                        labelText: "End date",
-                        labelStyle: TextStyle(color: Colors.black,
-                            fontStyle: FontStyle.normal, fontSize: 25),
-                        hintText: "Dec 02,2020",
-                        errorText: ('Start date should be greater than End date'),
-                      ),
-                      onTap:() async {
-                        DateTime? pickedDate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate:  DateTime(2000),
-                            //DateTime.now() - not to allow to choose before today.
-                            lastDate: DateTime(2101)
-                        );
-                        if(pickedDate != null){
-                          print(pickedDate);  //pickedDate output format => 2021-03-10 00:00:00.000
-                          String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate) ;
-                           // 2020-01-02 03:04:05.000
-
-                        print(formattedDate); //formatted date output using intl package =>  2021-03-16
-                          //you can implement different kind of Date Format here according to your requirement
-
-
-                          Enddate.text = formattedDate;
-                          // (Info > info);
-                           (context as Element).markNeedsBuild();//set output date to TextField value.
-
-                        }else{
-                          print("Date is not selected");
-                        }
-                      },
-                    ),
-                  ),
-
-
-                  new Align(
-                    alignment: Alignment.bottomRight,
-                    // ignore: deprecated_member_use
-                    child: RaisedButton(
-
-                        child: Text('Ok'),
-                        color: Colors.black,
-                        textColor: Colors.white,
-                        onPressed: () =>
-                            Navigator.of(context).push(
-                              CreatedTopicDetails.route(),)
-                    ),
-
-                  ),
-
-                ],
-              ),
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            CupertinoSliverNavigationBar(
+              largeTitle: Text('date of activity'),
             ),
-          )),
+          ];
+        },
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 50.0),
+                child: new TextField(
+                  controller: Startdate,
+                  decoration: InputDecoration(
+                    labelText: "Start date",
+                    labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 25),
+                    hintText: 'Dec 02,2020',
+                  ),
+                  onTap: () async {
+                    DateTime? PickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2101));
+                    if (PickedDate != null) {
+                      print(PickedDate);
+                      String formattedDate =
+                          DateFormat('yyyy-MM-dd').format(PickedDate);
+
+                      Startdate.text = formattedDate;
+
+                      (context as Element).markNeedsBuild();
+                    } else {
+                      print("Date is not selected");
+                    }
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 50.0, bottom: 200),
+                child: new TextField(
+                  controller: Enddate,
+                  decoration: InputDecoration(
+                    labelText: "End date",
+                    labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 25),
+                    hintText: "Dec 02,2020",
+                    errorText: ('Start date should be greater than End date'),
+                  ),
+                  onTap: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2101));
+                    if (pickedDate != null) {
+                      print(pickedDate);
+                      String formattedDate =
+                          DateFormat('yyyy-MM-dd').format(pickedDate);
+
+                      print(formattedDate);
+                      Enddate.text = formattedDate;
+                      (context as Element).markNeedsBuild();
+                    } else {
+                      print("Date is not selected");
+                    }
+                  },
+                ),
+              ),
+              new Align(
+                alignment: Alignment.bottomRight,
+                // ignore: deprecated_member_use
+                child: RaisedButton(
+                    child: Text('Ok'),
+                    color: Colors.black,
+                    textColor: Colors.white,
+                    onPressed: () => Navigator.of(context).push(
+                          CreatedTopicDetails.route(),
+                        )),
+              ),
+            ],
+          ),
+        ),
+      )),
     );
   }
-
-  getdata(info) {}
 }
-
-
 
 class CreatedTopicDetails extends StatelessWidget {
   static Route route() => MaterialPageRoute(
-    builder: (context) => CreatedTopicDetails(),
-  );
+        builder: (context) => CreatedTopicDetails(),
+      );
 
   @override
   Widget build(BuildContext context) {
